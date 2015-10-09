6934c56a6b881e71ef75977c47bd1bb61d3e6bbe
Tom Chen <txchen@gmail.com>
2015-10-09 12:21:25 -1000

    Create MSSQL_listCheckContraints_&_PartitionFunction.sql

---
+++ sql/MSSQL_listCheckContraints_&_PartitionFunction.sql
@@ -0,0 +1,16 @@
+-- From: http://www.sqlusa.com/bestpractices2005/listcheckconstraints/
+SELECT   TABLE_NAME,
+         COLUMN_NAME,
+         CHECK_CLAUSE,
+         cc.CONSTRAINT_SCHEMA,
+         cc.CONSTRAINT_NAME
+FROM     INFORMATION_SCHEMA.CHECK_CONSTRAINTS cc
+         INNER JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE c
+           ON cc.CONSTRAINT_NAME = c.CONSTRAINT_NAME
+ORDER BY CONSTRAINT_SCHEMA,
+         TABLE_NAME,
+         COLUMN_NAME
+GO
+
+-- See: https://msdn.microsoft.com/en-us/library/ms187381.aspx
+select * from sys.partition_functions 
